c054493412ec1d72230eac7b40ef7e7fab8f862d
Mostafizur Rahman <mostafizur0195@gmail.com>
2023-01-29 19:27:02 +0600

    React data flow

--- src/Pages/Child.js
+++ src/Pages/Child.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Child = () => {
-  return <div>Child</div>;
+const Child = ({ count, setCount }) => {
+  // const [count, setCount] = useState(0);
+  return (
+    <div>
+      {" "}
+      <div className="bg-gray-200 w-2/6 mx-auto my-5 py-5">
+        <div>
+          <h1 className="text-gray-400 text-xl font-bold">Child</h1>
+          <h1 className="text-xl text-red-600 mb-10">{count}</h1>
+          <div>
+            <button
+              onClick={() => setCount(count - 1)}
+              className="btn btn-info mx-5 capitalize"
+            >
+              Decrement
+            </button>
+            <button
+              onClick={() => setCount((prvState) => prvState + 1)}
+              className="btn btn-success capitalize"
+            >
+              Increment
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Child;
--- src/Pages/Parents.js
+++ src/Pages/Parents.js
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
+import Child from "./Child";
 
 const Parents = () => {
   const [count, setCount] = useState(0);
   return (
-    <div className="bg-gray-200 w-2/6 mx-auto my-5 py-5">
-      <div>
-        <h1 className="text-xl text-red-600 mb-10">{count}</h1>
+    <div>
+      <div className="bg-gray-200 w-2/6 mx-auto my-5 py-5">
         <div>
-          <button
-            onClick={() => setCount(count - 1)}
-            className="btn btn-info mx-5 capitalize"
-          >
-            Decrement
-          </button>
-          <button
-            onClick={() => setCount((prvState) => prvState + 1)}
-            className="btn btn-success capitalize"
-          >
-            Increment
-          </button>
+          <h1 className="text-gray-400 text-xl font-bold">Parants</h1>
+          <h1 className="text-xl text-red-600 mb-10">{count}</h1>
+          {/* <div>
+            <button
+              onClick={() => setCount(count - 1)}
+              className="btn btn-info mx-5 capitalize"
+            >
+              Decrement
+            </button>
+            <button
+              onClick={() => setCount((prvState) => prvState + 1)}
+              className="btn btn-success capitalize"
+            >
+              Increment
+            </button>
+          </div> */}
         </div>
       </div>
+      <Child count={count} setCount={setCount} />
     </div>
   );
 };
